8cbbe4a057e407ad4d14ee3263bb49edce2d20eb
klausyp45 <klaus.alusa@platform45.com>
2025-06-24 16:21:04 +0200

    add pundit and policies

---
+++ app/policies/post_policy.rb
@@ -0,0 +1,13 @@
+class PostPolicy < ApplicationPolicy
+  def create?
+    user.present?
+  end
+  
+  def update?
+    user.present? && (record.user == user || user.admin?)
+  end
+  
+  def destroy?
+    update?
+  end
+end
