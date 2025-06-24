f3bf2f63387d46f018944bf78e895081c67533a8
klausyp45 <klaus.alusa@platform45.com>
2025-06-24 16:18:32 +0200

    adding posts, somments, likes and page controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,33 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :posts
+  has_many :comments
+  has_many :likes
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+end
+
+# app/models/post.rb
+class Post < ApplicationRecord
+  belongs_to :user
+  has_many :comments, as: :commentable
+  has_many :likes, as: :likeable
+  
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :post_type, presence: true, inclusion: { in: %w[blog event news] }
+end
+
+# app/models/comment.rb
+class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
+  validates :body, presence: true
+end
+
+# app/models/like.rb
+class Like < ApplicationRecord
+  belongs_to :user
+  belongs_to :likeable, polymorphic: true
+  validates_uniqueness_of :user_id, scope: [:likeable_type, :likeable_id]
 end
