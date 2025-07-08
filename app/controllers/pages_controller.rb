a2e72ae204bcbc386f41df931a20f8914312eb51
klausyp45 <klaus.alusa@platform45.com>
2025-07-08 17:13:48 +0200

    adding unique layouts for pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,6 @@
 class PagesController < ApplicationController
+  layout :resolve_layout
+  
   def index
     @pages = Page.all
   end
@@ -7,5 +9,25 @@ class PagesController < ApplicationController
   end
   def show
     @page = Page.find_by!(slug: params[:id])
+  end
+
+  private
+
+  def resolve_layout
+    case action_name
+    when "show"
+      case params[:id]
+      when "about"
+        "about_layout"
+      when "contact"
+        "contact_layout"
+      when "pricing"
+        "pricing_layout"
+      else
+        "application" # default fallback
+      end
+    else
+      "application"
+    end
   end
 end
