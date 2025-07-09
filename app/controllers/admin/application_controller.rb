26973d6c545ad6a438563eadc9a80f7708da5d3d
klausyp45 <klaus.alusa@platform45.com>
2025-07-09 11:47:42 +0200

    restrict only admin to access the admin section

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,5 +1,16 @@
 module Admin
   class ApplicationController < Administrate::ApplicationController
-    # You can add authentication here if needed
+    before_action :authenticate_user!
+    before_action :authorize_admin!
+
+    private
+
+    def authorize_admin!
+      unless current_user.admin?
+        flash[:alert] = "You are not authorized to access this section."
+        redirect_to root_path
+      end
+    end
+
   end
 end
